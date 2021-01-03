73149d50052d99bf16a5dbd300be8e1a16df8801
Saptarshi Basu <sbasu2324@gmail.com>
2021-01-03 14:56:10 +0530

    removing to string circular reference.

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -78,7 +78,6 @@ public class Author {
                 "id=" + id +
                 ", firstname='" + firstname + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + books +
                 '}';
     }
 }
